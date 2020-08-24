949f275fb7749464c3cc8d07398c95b9611ce03f
rictaaa <maxrichterr88@gmail.com>
2020-08-24 16:42:16 -0500

    formatted DataSection

--- wiscovidapp/src/components/DataSection.jsx
+++ wiscovidapp/src/components/DataSection.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import MapSection from './MapSection';
-import axios from 'axios';
-import { GrBug, GrScorecard, GrGroup, GrStatusCritical, GrStatusGood, GrBarChart } from 'react-icons/gr';
-import './map-theme';
+import MapSection from "./MapSection";
+import axios from "axios";
+import {
+  GrBug,
+  GrScorecard,
+  GrGroup,
+  GrStatusCritical,
+  GrStatusGood,
+  GrBarChart,
+} from "react-icons/gr";
+import "./map-theme";
 
 class DataSection extends React.Component {
-
   // keep track of object's state
   state = {
     countyData: {
@@ -15,9 +21,9 @@ class DataSection extends React.Component {
       posNew: 0,
       negNew: 0,
       testNew: 0,
-      name: '',
-      dailyChange: 0
-    } 
+      name: "",
+      dailyChange: 0,
+    },
   };
 
   /**
@@ -25,29 +31,36 @@ class DataSection extends React.Component {
    * receives data from the default county of DANE
    */
   componentDidMount() {
-    let baseURL = "https://services1.arcgis.com/ISZ89Z51ft1G16OK/ArcGIS/rest/services/COVID19_WI/FeatureServer/10/query?where=NAME=" + 
-    "%27"+ this.props.currCounty + "%27&outFields=NEGATIVE,POSITIVE,DEATHS,POS_NEW,NEG_NEW,TEST_NEW,NAME&outSR=4326&f=json"
-    axios.get(baseURL).then(data => {  
-      let respData;
-      let temp = data.data.features;
-      let currLength = temp.length - 1;
-      respData = temp[currLength].attributes;
-      this.setState( prevState => {
-        let countyData = Object.assign({}, prevState.countyData);
-        countyData['negative'] = respData.NEGATIVE;
-        countyData['positive'] = respData.POSITIVE;
-        countyData['deaths'] = respData.DEATHS;
-        countyData['posNew'] = respData.POS_NEW;
-        countyData['negNew'] = respData.NEG_NEW;
-        countyData['testNew'] = respData.TEST_NEW;
-        countyData['name'] = respData.NAME;
-        countyData['dailyChange'] = respData.POS_NEW - temp[currLength-1].attributes.POS_NEW;
-        return { countyData };
-      });  
-    }).catch(error => {
-      alert("Something went wrong, please reload the page");
-    });
-  };
+    let baseURL =
+      "https://services1.arcgis.com/ISZ89Z51ft1G16OK/ArcGIS/rest/services/COVID19_WI/FeatureServer/10/query?where=NAME=" +
+      "%27" +
+      this.props.currCounty +
+      "%27&outFields=NEGATIVE,POSITIVE,DEATHS,POS_NEW,NEG_NEW,TEST_NEW,NAME&outSR=4326&f=json";
+    axios
+      .get(baseURL)
+      .then((data) => {
+        let respData;
+        let temp = data.data.features;
+        let currLength = temp.length - 1;
+        respData = temp[currLength].attributes;
+        this.setState((prevState) => {
+          let countyData = Object.assign({}, prevState.countyData);
+          countyData["negative"] = respData.NEGATIVE;
+          countyData["positive"] = respData.POSITIVE;
+          countyData["deaths"] = respData.DEATHS;
+          countyData["posNew"] = respData.POS_NEW;
+          countyData["negNew"] = respData.NEG_NEW;
+          countyData["testNew"] = respData.TEST_NEW;
+          countyData["name"] = respData.NAME;
+          countyData["dailyChange"] =
+            respData.POS_NEW - temp[currLength - 1].attributes.POS_NEW;
+          return { countyData };
+        });
+      })
+      .catch((error) => {
+        alert("Something went wrong, please reload the page");
+      });
+  }
 
   /**
    * gets new covid data as user selects a different county
@@ -55,57 +68,73 @@ class DataSection extends React.Component {
    * is that this will be called only when the state updates
    */
   componentDidUpdate() {
-      let baseURL = "https://services1.arcgis.com/ISZ89Z51ft1G16OK/ArcGIS/rest/services/COVID19_WI/FeatureServer/10/query?where=NAME=" + 
-    "%27"+ this.props.currCounty + "%27&outFields=NEGATIVE,POSITIVE,DEATHS,POS_NEW,NEG_NEW,TEST_NEW,NAME&outSR=4326&f=json"
-    axios.get(baseURL).then(data => {  
-      let respData;
-      let temp = data.data.features;
-      let currLength = temp.length - 1;
-      respData = temp[currLength].attributes;
-      this.setState( prevState => {
-        let countyData = Object.assign({}, prevState.countyData);
-        countyData['negative'] = respData.NEGATIVE;
-        countyData['positive'] = respData.POSITIVE;
-        countyData['deaths'] = respData.DEATHS;
-        countyData['posNew'] = respData.POS_NEW;
-        countyData['negNew'] = respData.NEG_NEW;
-        countyData['testNew'] = respData.TEST_NEW;
-        countyData['name'] = respData.NAME;
-        countyData['dailyChange'] = respData.POS_NEW - temp[currLength-1].attributes.POS_NEW;
-        return { countyData };
-      });  
-    }).catch(error => {
-      alert("Something went wrong, please reload the page");
-    });
-  };
-
+    let baseURL =
+      "https://services1.arcgis.com/ISZ89Z51ft1G16OK/ArcGIS/rest/services/COVID19_WI/FeatureServer/10/query?where=NAME=" +
+      "%27" +
+      this.props.currCounty +
+      "%27&outFields=NEGATIVE,POSITIVE,DEATHS,POS_NEW,NEG_NEW,TEST_NEW,NAME&outSR=4326&f=json";
+    axios
+      .get(baseURL)
+      .then((data) => {
+        let respData;
+        let temp = data.data.features;
+        let currLength = temp.length - 1;
+        respData = temp[currLength].attributes;
+        this.setState((prevState) => {
+          let countyData = Object.assign({}, prevState.countyData);
+          countyData["negative"] = respData.NEGATIVE;
+          countyData["positive"] = respData.POSITIVE;
+          countyData["deaths"] = respData.DEATHS;
+          countyData["posNew"] = respData.POS_NEW;
+          countyData["negNew"] = respData.NEG_NEW;
+          countyData["testNew"] = respData.TEST_NEW;
+          countyData["name"] = respData.NAME;
+          countyData["dailyChange"] =
+            respData.POS_NEW - temp[currLength - 1].attributes.POS_NEW;
+          return { countyData };
+        });
+      })
+      .catch((error) => {
+        alert("Something went wrong, please reload the page");
+      });
+  }
 
   render() {
-
     /**
      * decide whether or not change in daily positive cases is positive or negative
      * add appropriate '+ or -' depending on the value
      */
     let caseChange;
     if (this.state.countyData.dailyChange > 0) {
-      caseChange = <span className="card-data-style">{'+' + this.state.countyData.dailyChange.toLocaleString()}</span>;
+      caseChange = (
+        <span className="card-data-style">
+          {"+" + this.state.countyData.dailyChange.toLocaleString()}
+        </span>
+      );
     } else {
-      caseChange = <span className="card-data-style">{this.state.countyData.dailyChange.toLocaleString()}</span>;
+      caseChange = (
+        <span className="card-data-style">
+          {this.state.countyData.dailyChange.toLocaleString()}
+        </span>
+      );
     }
-    
+
     return (
-      
       <div className="container-fluid">
         {/* FIRST ROW */}
         <div className="row justify-content-center row-styled">
           <div className="col-lg-3 col-sm-6">
             <div className="card grid-card">
               <div className="card-heading">
-                <div className="card-title-style">Positive Cases (Total) <GrBug className="icon-styled" size="1.5rem"/></div>
-                
+                <div className="card-title-style">
+                  Positive Cases (Total){" "}
+                  <GrBug className="icon-styled" size="1.5rem" />
+                </div>
               </div>
               <div className="card-value">
-                <span className="card-data-style">{this.state.countyData.positive.toLocaleString()}</span>
+                <span className="card-data-style">
+                  {this.state.countyData.positive.toLocaleString()}
+                </span>
               </div>
             </div>
           </div>
@@ -113,11 +142,16 @@ class DataSection extends React.Component {
           <div className="col-lg-3 col-sm-6">
             <div className="card grid-card">
               <div className="card-heading">
-                <div className="card-title-style">Negative Cases (Total) <GrScorecard className="icon-styled" size="1.5rem"/></div>  
+                <div className="card-title-style">
+                  Negative Cases (Total){" "}
+                  <GrScorecard className="icon-styled" size="1.5rem" />
+                </div>
               </div>
-              
+
               <div className="card-value">
-                <span className="card-data-style">{this.state.countyData.negative.toLocaleString()}</span>
+                <span className="card-data-style">
+                  {this.state.countyData.negative.toLocaleString()}
+                </span>
               </div>
             </div>
           </div>
@@ -125,10 +159,14 @@ class DataSection extends React.Component {
           <div className="col-lg-3 col-sm-6">
             <div className="card grid-card">
               <div className="card-heading">
-                <div className="card-title-style">Total Deaths <GrGroup className="icon-styled" size="1.5rem"/></div>
+                <div className="card-title-style">
+                  Total Deaths <GrGroup className="icon-styled" size="1.5rem" />
+                </div>
               </div>
               <div className="card-value">
-                <span className="card-data-style">{this.state.countyData.deaths.toLocaleString()}</span>
+                <span className="card-data-style">
+                  {this.state.countyData.deaths.toLocaleString()}
+                </span>
               </div>
             </div>
           </div>
@@ -139,10 +177,15 @@ class DataSection extends React.Component {
           <div className="col-lg-3 col-sm-6">
             <div className="card grid-card">
               <div className="card-heading">
-                <div className="card-title-style">New Positive Cases (Daily) <GrStatusCritical className="icon-styled" size="1.5rem"/></div>
+                <div className="card-title-style">
+                  New Positive Cases (Daily){" "}
+                  <GrStatusCritical className="icon-styled" size="1.5rem" />
+                </div>
               </div>
               <div className="card-value">
-                <span className="card-data-style">{this.state.countyData.posNew.toLocaleString()}</span>
+                <span className="card-data-style">
+                  {this.state.countyData.posNew.toLocaleString()}
+                </span>
               </div>
             </div>
           </div>
@@ -150,10 +193,15 @@ class DataSection extends React.Component {
           <div className="col-lg-3 col-sm-6">
             <div className="card grid-card">
               <div className="card-heading">
-                <div className="card-title-style">New Negative Cases (Daily) <GrStatusGood className="icon-styled" size="1.5rem"/></div>
+                <div className="card-title-style">
+                  New Negative Cases (Daily){" "}
+                  <GrStatusGood className="icon-styled" size="1.5rem" />
+                </div>
               </div>
               <div className="card-value">
-                <span className="card-data-style">{this.state.countyData.negNew.toLocaleString()}</span>
+                <span className="card-data-style">
+                  {this.state.countyData.negNew.toLocaleString()}
+                </span>
               </div>
             </div>
           </div>
@@ -161,11 +209,12 @@ class DataSection extends React.Component {
           <div className="col-lg-3 col-sm-6">
             <div className="card grid-card">
               <div className="card-heading">
-                <div className="card-title-style">Change in Daily Positive Cases <GrBarChart className="icon-styled" size="1.5rem"/></div>
+                <div className="card-title-style">
+                  Change in Daily Positive Cases{" "}
+                  <GrBarChart className="icon-styled" size="1.5rem" />
+                </div>
               </div>
-              <div className="card-value">
-                {caseChange}
-              </div>
+              <div className="card-value">{caseChange}</div>
             </div>
           </div>
         </div>
