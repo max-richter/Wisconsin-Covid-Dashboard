89aa455898166bbaf0b7b8f74bdb17ed76c9b2dd
rictaaa <maxrichterr88@gmail.com>
2020-09-08 17:52:07 -0500

    Updates

---
+++ precache-manifest.fc25bd44be35a8ad3a73ac89279fe2ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e6b10d500dad0b0ceee01c5a14d4e5",
+    "url": "/Wisconsin-Covid-Dashboard/index.html"
+  },
+  {
+    "revision": "ca6e3d38831a4f897a01",
+    "url": "/Wisconsin-Covid-Dashboard/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "17d5d440c4492d54a309",
+    "url": "/Wisconsin-Covid-Dashboard/static/css/main.0fbef9f3.chunk.css"
+  },
+  {
+    "revision": "ca6e3d38831a4f897a01",
+    "url": "/Wisconsin-Covid-Dashboard/static/js/2.7944675d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Wisconsin-Covid-Dashboard/static/js/2.7944675d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17d5d440c4492d54a309",
+    "url": "/Wisconsin-Covid-Dashboard/static/js/main.ac0905bc.chunk.js"
+  },
+  {
+    "revision": "cafa2fd799ff3498418f",
+    "url": "/Wisconsin-Covid-Dashboard/static/js/runtime-main.4370df6b.js"
+  }
+]);
